2314eeb1488353c7b8c6ed29f2468fb461bbe18b
Moritz Fink <moritz.fink@accenture.com>
2023-01-07 14:05:43 +0100

    Add second plot with histogram

--- etf_simulator.py
+++ etf_simulator.py
@@ -1,5 +1,6 @@
 import click
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 
 def simulate(annual_etf_gross_returns_average, annual_etf_gross_returns_sigma, annual_inflation_average, annual_inflation_sigma, capital_start, max_years, annual_savings_rate_start, annual_savigns_rate_increase):
@@ -28,7 +29,7 @@ def simulate(annual_etf_gross_returns_average, annual_etf_gross_returns_sigma, a
 @click.option('--annual_etf_gross_returns_sigma', default=0.1452, help='Standard deviation of the relative average of the ETF\'s annual gross returns')
 @click.option('--annual_inflation_average', default=0.0245, help='Relative average of inflation')
 @click.option('--annual_inflation_sigma', default=0.011343, help='Standard deviation of the relative average of inflation')
-@click.option('--number_of_simulations', default=1000, help='Number of simulations')
+@click.option('--number_of_simulations', default=100, help='Number of simulations')
 @click.option('--capital_start', default=40000, help='Capital at the start of each simulation')
 @click.option('--max_years', default=25, help='Number of years to be simulated')
 @click.option('--annual_savings_rate_start', default=12 * 1700, help='Annual savings at the start of each simulation. This amount will be added to the portfolio at the end of each year')
@@ -41,20 +42,26 @@ def main(annual_etf_gross_returns_average, annual_etf_gross_returns_sigma, annua
         
     capital_per_year_per_simulation.sort(key=lambda x:x[max_years])
     
-    for i in range(number_of_simulations):
-        plt.plot(range(max_years + 1), capital_per_year_per_simulation[i], color = "lightgrey", linestyle = ":")
+    fig, plots = plt.subplots(2, constrained_layout=True)
     
-    plt.plot(range(max_years + 1), capital_per_year_per_simulation[number_of_simulations - 1], color = "green", linestyle = "--", label = "Best case (" + "{:,}".format(int(capital_per_year_per_simulation[number_of_simulations - 1][max_years])) + ")")
-    plt.plot(range(max_years + 1), capital_per_year_per_simulation[int(number_of_simulations / 2)], color = "orange", linestyle = "-", label = "Median case (" + "{:,}".format(int(capital_per_year_per_simulation[int(number_of_simulations / 2)][max_years])) + ")")
-    plt.plot(range(max_years + 1), capital_per_year_per_simulation[0], color = "red", linestyle = "--", label = "Worst case (" + "{:,}".format(int(capital_per_year_per_simulation[0][max_years])) + ")")
+    for i in range(number_of_simulations):
+        plots[0].plot(range(max_years + 1), capital_per_year_per_simulation[i], color = "lightgrey", linestyle = ":")
+    
+    plots[0].plot(range(max_years + 1), capital_per_year_per_simulation[number_of_simulations - 1], color = "green", linestyle = "--", label = "Best case (" + "{:,}".format(int(capital_per_year_per_simulation[number_of_simulations - 1][max_years])) + ")")
+    plots[0].plot(range(max_years + 1), capital_per_year_per_simulation[int(number_of_simulations / 2)], color = "orange", linestyle = "-", label = "Median case (" + "{:,}".format(int(capital_per_year_per_simulation[int(number_of_simulations / 2)][max_years])) + ")")
+    plots[0].plot(range(max_years + 1), capital_per_year_per_simulation[0], color = "red", linestyle = "--", label = "Worst case (" + "{:,}".format(int(capital_per_year_per_simulation[0][max_years])) + ")")
     
     invested_capital_per_year = [(capital_start + i * annual_savings_rate_start * pow(1.0 + annual_savigns_rate_increase, i)) * pow(1.0 - annual_inflation_average, i) for i in range(max_years + 1)]
-    plt.plot(range(max_years + 1), invested_capital_per_year, color = "blue", linestyle = "-.", label = "Invested (" + "{:,}".format(int(invested_capital_per_year[max_years])) + ")")
+    plots[0].plot(range(max_years + 1), invested_capital_per_year, color = "blue", linestyle = "-.", label = "Invested (" + "{:,}".format(int(invested_capital_per_year[max_years])) + ")")
     
-    plt.title("ETF")
-    plt.xlabel("Years")
-    plt.ylabel("Capital (after inflation)")
-    plt.legend()
+    plots[0].set_title("Portfolio development")
+    plots[0].set(xlabel = "Years", ylabel="Capital (after inflation)")
+    plots[0].legend()
+    
+    counts, bins = np.histogram([capital_per_year_per_simulation[i][max_years] for i in range(number_of_simulations)], bins=int(0.1 * number_of_simulations))
+    plots[1].stairs(counts, bins)
+    plots[1].set_title("Final capital")
+    plots[1].set(xlabel = "Capital (after inflation)", ylabel="Number of simulations")
     
     plt.show()
 
